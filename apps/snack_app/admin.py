ee0d2d38dd225b392ed64a682b3caa729691b13e
unknown <morreydson@gmail.com>
2018-04-24 05:40:32 +0800

    setting up the admin.py file

--- apps/snack_app/admin.py
+++ apps/snack_app/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+# from . models import Article#importing our article from the models file
+
+# # Register your models here.
+# admin.site.register(Article)#asking django to register sth on the admin site
+
